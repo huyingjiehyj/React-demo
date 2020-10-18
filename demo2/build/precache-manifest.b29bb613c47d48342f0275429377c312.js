d875e534299791b4dc787c20c3375abcb9019c34
huyingjiehyj <2691408920@qq.com>
2020-10-18 16:37:36 +0800

    Signed-off-by: huyingjiehyj <2691408920@qq.com>

---
+++ demo2/build/precache-manifest.b29bb613c47d48342f0275429377c312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f52c2648b22a80127f835825c79dd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9bc870cf6676e925c93",
+    "url": "/static/css/main.bf052a61.chunk.css"
+  },
+  {
+    "revision": "5d07533491404da85077",
+    "url": "/static/js/2.36fe17ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.36fe17ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9bc870cf6676e925c93",
+    "url": "/static/js/main.c0ac25bd.chunk.js"
+  },
+  {
+    "revision": "efc8d4bc350e30b3cd92",
+    "url": "/static/js/runtime-main.4252b490.js"
+  }
+]);
