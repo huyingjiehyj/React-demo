1984a6d6c28c8c38a8f2fe8a04e1f2b5fb90b957
huyingjiehyj <2691408920@qq.com>
2020-10-25 15:58:44 +0800

    1

---
+++ demo3/build/precache-manifest.801fb89d1a7d8e94d65b2ff1d6428c87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4340da05015545f130a8ece3c8fa15ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dabf2084b9838bab7fb",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "80c3c0770942d04752ec",
+    "url": "/static/css/main.56d3c4a7.chunk.css"
+  },
+  {
+    "revision": "3dabf2084b9838bab7fb",
+    "url": "/static/js/2.67db91e9.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.67db91e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c3c0770942d04752ec",
+    "url": "/static/js/main.33c1138d.chunk.js"
+  },
+  {
+    "revision": "efc8d4bc350e30b3cd92",
+    "url": "/static/js/runtime-main.4252b490.js"
+  }
+]);
